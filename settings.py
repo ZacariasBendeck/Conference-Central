d3daf96acbaa97321b7912bbde6daab4b2243aaf
ZacariasBendeck <zbendeck@gmail.com>
2016-01-25 02:29:25 -0600

    Initial Commit:
    Cloned the Conference central repository and worked on the following
    things:
    
    1. Added Session, SessionForm, and SessionForms classes to models.py
    2. Added createSession endpoints method and it is working.
    3. Started working on getConferenceSessions but this is not working.
    4. Added copySessionToForm function as well.
    
    Problem is in line 693. Sessions objects are not being retrieved!

---
+++ settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '1072294616917-38v31atft6n19ekuhpqctp5jnq90bb1p.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = 'AIzaSyCiblYJ1oRPo9RF10JI38kFiyx2aqdf2Cs'
+IOS_CLIENT_ID = 'AIzaSyCWnIwrwTnIE8y5Hv1hfXJCWGiOP-Mhmsw'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
